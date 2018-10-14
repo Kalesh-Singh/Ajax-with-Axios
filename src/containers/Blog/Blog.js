ee3a5f7c87785b059c322a1a1e5cb3238147c0c1
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-10-14 16:27:52 -0700

    Retrieving data for a single post via the componentDidUpdate lifecycle hook

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     };
 
     componentDidMount() {
@@ -23,12 +24,17 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    };
+
     render () {
         const posts = this.state.posts.map(post =>
                 (<Post
                     key={post.id}
                     title={post.title}
                     author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}
                 />));
 
         return (
@@ -37,7 +43,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
