f0b85e8ef8d4f2c3ef22ffae6900d5163e75908b
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-10-14 15:37:54 -0700

    Transformed retrieved data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -15,12 +15,21 @@ class Blog extends Component {
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data});
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => (
+                    {...post, author: 'Kalesh'}
+                ));
+                this.setState({posts: updatedPosts});
             });
     }
 
     render () {
-        const posts = this.state.posts.map(post => (<Post key={post.id} title={post.title}/>));
+        const posts = this.state.posts.map(post =>
+                (<Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                />));
 
         return (
             <div>
