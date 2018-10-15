305b840771264db2a657e1a7ea5bbde3172bac1a
Kalesh Singh <kaleshwar.singh@bison.howard.edu>
2018-10-14 17:04:33 -0700

    Axios interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(requestConfig => {
+    console.log(requestConfig);
+    // We can edi the request config here.
+    return requestConfig;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(responseConfig => {
+    console.log(responseConfig);
+    // We can edi the request config here.
+    return responseConfig;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
